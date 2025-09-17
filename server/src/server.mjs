eb668f426fb951f1af89c84178656d76ffb0bccb
vukverovic <vukverovic@gmail.com>
2025-09-17 22:57:44 +0200

    package i package lock

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -1,3 +1,3 @@
 import app from "./src/app.mjs";
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Secret Santa backend on :${PORT}`));
+app.listen(PORT, () => console.log(`Kucni budzet backend on :${PORT}`));
